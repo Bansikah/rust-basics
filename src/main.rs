05c4ae4da2902cf3e03e67093f93dd3ab48c4884
Bansikah <tandapnoelbansikah@gmail.com>
2024-03-11 09:39:08 +0100

    lifetime

--- src/main.rs
+++ src/main.rs
@@ -218,7 +218,29 @@ for aircraft in aircraft_array{
     
  }
  println!("\nThe original value is: {}", orignal);
- 
+
+
+ //life time, applies to items only stored in a heap
+ let reference_int = 6;
+ let return_value = return_one_parameter(&reference_int);
+ println!("The return value is: {}", return_value);
+
+ let value_one = 45;
+ let value_two = 30;
+ let value = explicit_lifetme(&value_one, &value_two);
+
 }
 
+fn return_one_parameter(value: &i32) -> &i32{
+    value
+}
 
+//life time example two
+fn expelicit_lifetme<'a>(p1: &'a i32, p2: &'a i32) -> &'a i32{
+    if p1 > p2 {
+        p1
+    }else {
+        p2
+    }
+      
+}
